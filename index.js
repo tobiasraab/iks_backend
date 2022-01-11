2be54818c89943c7b89f65287c9fd7f103e26075
tobiasraab <64197026+tobiasraab@users.noreply.github.com>
2022-01-11 12:10:49 +0100

    update

--- index.js
+++ index.js
@@ -14,20 +14,67 @@ const port = 3001;
 // Serial Monitor
 const SerialPort = require('serialport')
 const Readline = SerialPort.parsers.Readline
-const port = new SerialPort("COM4")
+const portSerial = new SerialPort("COM4")
 SerialPort.baudRate = 9600
 const parser = new Readline()
-port.pipe(parser)
+portSerial.pipe(parser)
 
 // state of muenster
-let state = ['active', 'active', 'active', 'active', 'active', 'active', 'active', 'active', 'active', 'active', 'active', 'active']
-
+let state = ['passive', 'passive', 'passive', 'passive', 'passive', 'passive', 'passive', 'passive', 'passive', 'passive', 'passive', 'passive']
+let lastState = ['passive', 'passive', 'passive', 'passive', 'passive', 'passive', 'passive', 'passive', 'passive', 'passive', 'passive', 'passive']
+let focusState = undefined
 /* Serialport parser */
 let lastMsg = undefined
 parser.on('data', (data)=>{
   if(data != lastMsg || lastMsg === undefined){
     let msg  = JSON.parse(data.toString())
     console.log("SP_INCOMING_MSG: ", msg)
+
+    // set state
+    if(msg.towerBottomPieceActive === 1){
+      if(lastState[0] === 'passive'){
+        state[0] = 'focus'
+      }
+      else {
+        state[0] = 'active'
+      }
+      lastState[0] = state[0]
+    }
+    else if(msg.towerBottomPieceActive === 0){
+      state[0] = 'passive'
+    }
+
+    if(msg.towerTopPieceActive === 1){
+      if(lastState[1] === 'passive'){
+        state[1] = 'focus'
+      }
+      else {
+        state[1] = 'active'
+      }
+    }
+    else if(msg.towerTopPieceActive === 0){
+      state[1] = 'passive'
+    }
+
+    // check focus
+    focusState = false
+    for(let i=0; i < state.length; i++){
+      if(state[i]==='focus'){
+        focusState = true
+      }
+    }
+    console.log(focusState)
+    if(focusState === false){
+      for(let i = state.length; i >= 0; i--){
+        if(state[i] === 'active'){
+          state[i] = 'focus'
+          break;
+        }
+      }
+    }
+    lastState = state
+
+    io.emit('data', state)
   }
   lastMsg = data
 })
